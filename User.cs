7707ae5b502e41a193779b2c4e7554c730388107
LukWen-Ill <Lukaswennstrom@hotmail.com>
2025-10-13 13:25:59 +0200

    Removed Admin class and adding list of permission into User

--- User.cs
+++ User.cs
@@ -2,28 +2,31 @@ namespace App;
 // User class - A new user.
 class User
 // All Users have: 
-// UserID.
+// UserID. will be set dynamicly
 // Username. 
 // Password.
 // Role is defaulted to Role.User.
-// Location is defaulted to Location.Hospital.
+// Location is defaulted to Hospital.
+// Region is defaulted to Region.
 {
-    public int UserID;
+    public int UserID; // DEV: needs to be autoset via function GetUserID() (måste va en float())
     public string Username;
     public string Password;
     public Role UserRole;
     public Location AssignedLocation;
-
+    public Region AssignedRegion;
+    public List<Permission> permissions = new List<Permission>();
 
     //  Constructor
-    public User(int userID, string username, string password)
+    public User(string username, string password)
     // When a new User is created the variables taken in are UserID, Username, Password.
     {
-        UserID = userID;
+        UserID = SetID(); // DEV hardcoded until function SetID() done
         Username = username;
         Password = password;
-        UserRole = Role.User                    // User default get role User.
+        UserRole = Role.User;                   // User default get role User.
         AssignedLocation = Location.Hospital;   // Hardcoded location to Hospital.
+        AssignedRegion = Region.Region;         // Hardcode Region
 
     }
 
@@ -38,5 +41,13 @@ class User
         Admin,
         Staff,
         Patient,
+    }
+    enum Permissions
+    {
+
+    }
+    public UInt32 SetID() // Unsigned int - goes from 0-32bit
+    {
+        return;
     }
 }
