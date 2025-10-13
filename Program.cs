e00b823a7f9f6823c378bf6169e93e0219057270
LukWen-Ill <Lukaswennstrom@hotmail.com>
2025-10-13 13:58:17 +0200

    added testuser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using App;
+
+User testUser = new User("Lukas", "1");
+
+List<User> users = new List<User>();
+
+users.Add(testUser);
+
+User? active_user = null;
+
+Console.WriteLine("1. register");
+Console.WriteLine("2. login");
+
+string? input = Console.ReadLine();
