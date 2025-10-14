2adfe9c00a4dba94517e3745fb075cf1c8fa9bd9
Zhaneta-Lecini <zhaneta.lecini@student.nbi-handelsakademin.se>
2025-10-14 12:07:11 +0200

    Skapat en lista med alla permissions som Rollen ska ha, t.ex Admin kan HandlePermissions
     Add en Konstrukt som ska tar in rollens namn och skapar en tom lista , för permissions,

--- Role.cs
+++ Role.cs
@@ -1,4 +1,26 @@
-public class Role
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+/*
+        HandlePermissions,          // Tillåter att hantera andra användares rättigheter (ge/tar bort permissions)
+        AssignAdminsToRegions,      // Kan tilldela admin-användare till specifika regioner
+        HandleRegistrations,        // Kan godkänna/ neka patientregistreringar
+        AddLocations,               // Tillåter att lägga till nya sjukhus/kliniker/platser i systemet
+        CreatePersonnelAccounts,    // Tillåter att skapa konton för personal (läkare, sköterskor etc.)
+        ViewPermissionsList,        // Kan se vilka användare har vilka rättigheter
+        ViewPatientJournal,         // kan läsa patientjournaler
+        ModifyJournalPermissions,   // Tillåter att sätta olika läsrättigheter på journaler
+        RegisterAppointments,       // Tillåter att registrera nya patientbokningar
+        ModifyAppointments,         // Tillåter att ändra eller uppdatera befintliga bokningar
+        ApproveAppointments,        // Tillåter att godkänna patienters bokningsförfrågningar
+        ViewSchedule     
+        */
+public class Role //detta är en klass som rapresenterar en roll på systemet, "Admin", "Patient" eller "Personnel"
 {
-    
+    public string name;// namn på rollen ("patient, "Dr." eller "Ptient")
+    public List<Permission> permissions;// vi skapar en lista med alla rättigheter som denna roll har. För exampel, en Admin kan ha Addlokation, Handle permission etc..
+
+    public Role(string roleName)
+    {
+        n
+    }
 }
